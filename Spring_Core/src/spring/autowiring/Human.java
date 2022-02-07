1848825788685805c4b6d06393cc62ac816ed943
गौरव_बस्याल <bhishmapitamah@pop-os.localdomain>
2022-02-07 12:45:22 +0545

    qualifier_also_added

--- Spring_Core/src/spring/autowiring/Human.java
+++ Spring_Core/src/spring/autowiring/Human.java
@@ -1,26 +1,34 @@
 package spring.autowiring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Human {
-	Heart heart;
-	
-	public Human() {
-		
-	}
+
 	@Autowired
+	@Qualifier("humanHeart") // HERE I AM EXPLICITLY TELLING SPRING TO MAKE AN OBJECT FOR humanHeart type
+	Heart heart;
+
+	public Human() {
+
+	}
+
+	@Autowired
+	@Qualifier("humanHeart") // HERE I AM EXPLICITLY TELLING SPRING TO MAKE AN OBJECT FOR humanHeart type
 	public void setHeart(Heart heart) {
 		this.heart = heart;
 	}
-	
+
 	public Human(Heart heart) {
-		this.heart=heart;
+		this.heart = heart;
 	}
 
 	public void alive() {
 
 		if (heart != null) {
 			heart.pump();
+			System.out.println("The animal name is " + heart.getNameOfAnimal() + " the number of heart present is "
+					+ heart.getNoOfHeart());
 		} else {
 			System.out.println("You are dead");
 		}
