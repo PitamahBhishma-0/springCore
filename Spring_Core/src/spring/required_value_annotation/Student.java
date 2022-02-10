2ec14a9da35e706512093b7654b0356933ac0327
गौरव बस्याल <gauravbasyal789@gmail.com>
2022-02-10 12:43:07 +0545

    value&required_annotation

--- Spring_Core/src/spring/required_value_annotation/Student.java
+++ Spring_Core/src/spring/required_value_annotation/Student.java
@@ -1,38 +1,42 @@
 package spring.required_value_annotation;
 
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Value;
+
+@SuppressWarnings("deprecation")
 public class Student {
+	// @Value("Gaurav")//------------------- Defining static value
+	@Value("${student.name}") // ------------------ defining dynamic value USING .properties file
 	private String name;
+	// @Value("JAVA")
+	@Value("${student.interestedCourse}")
 	private String interestedCourse;
+	// @Value("Coding")
+	@Value("${student.hobby}")
 	private String hobby;
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+	/*
+	 * @Value("${student.name}") public void setName(String name) { this.name =
+	 * name; System.out.println("setName method called");
+	 * 
+	 * }
+	 * 
+	 * @Value("${student.interestedCourse}") public void setInterestedCourse(String
+	 * interestedCourse) { this.interestedCourse = interestedCourse;
+	 * System.out.println("setInterested method called"); }
+	 * 
+	 * @Value("${student.hobby}") public void setHobby(String hobby) { this.hobby =
+	 * hobby; System.out.println("setHobby method called"); }
+	 */
 
 	public void displayInfo() {
-		System.out.println("The name of student is"+ name);
-		System.out.println("The name of Interested course is"+ interestedCourse);
-		System.out.println("The hobby is of student"+ hobby);
-	}
-
-	public String getInterestedCourse() {
-		return interestedCourse;
-	}
-
-	public void setInterestedCourse(String interestedCourse) {
-		this.interestedCourse = interestedCourse;
-	}
-
-	public String getHobby() {
-		return hobby;
-	}
-
-	public void setHobby(String hobby) {
-		this.hobby = hobby;
+		System.out.println("The name of student is " + name);
+		System.out.println("The name of Interested course is " + interestedCourse);
+		System.out.println("The hobby is of student " + hobby);
 	}
 
 }
